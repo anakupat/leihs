0617bd644876ce69bcbbea5951be53bbdecb8564
Matúš Kmiť <matus.kmit@zhdk.ch>
2016-03-11 14:11:55 +0100

    revert previous commits regarding screenshots

--- spec/turnip_helper.rb
+++ spec/turnip_helper.rb
@@ -30,41 +30,14 @@ RSpec.configure do |config|
     end
   end
 
-  if ENV['CIDER_CI_TRIAL_ID'].present?
-    config.before(type: :feature) do
-      page.driver.browser.manage.window.maximize
-    end
-
-    config.after(type: :feature) do
-      unless example.exception.nil?
-        take_screenshot
-      end
-    end
-  end
-
   config.before(type: :feature) do
     FactoryGirl.create(:setting) unless Setting.first
+
     Capybara.current_driver = :firefox
   end
 
   config.after(type: :feature) do
     page.driver.quit # OPTIMIZE force close browser popups
     Capybara.current_driver = Capybara.default_driver
-  end
-
-  def take_screenshot(screenshot_dir = nil, name = nil)
-    screenshot_dir ||= Rails.root.join('tmp', 'capybara')
-    name ||= "screenshot_#{Time.zone.now.iso8601.gsub(/:/, '-')}.png"
-    Dir.mkdir screenshot_dir rescue nil
-    path = screenshot_dir.join(name)
-    case Capybara.current_driver
-    when :firefox
-      page.driver.browser.save_screenshot(path) rescue nil
-    else
-      Rails
-        .logger
-        .warn "Taking screenshots is not implemented for \
-      #{Capybara.current_driver}."
-    end
   end
 end
